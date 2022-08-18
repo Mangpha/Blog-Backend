6e2c7b6f04b4042fcf0081ddab2dbb66626be987
Mangpha <8350130@gmail.com>
2022-08-18 14:37:30 +0900

    # Feature / Edit Post

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InternalServerErrorOutput } from 'src/common/common.error';
 import { User } from 'src/user/entities/user.entity';
 import { CreatePostInput, CreatePostOutput } from './dtos/createPost.dto';
+import { EditPostInput, EditPostOutput } from './dtos/editPost.dto';
 import { FindAllPostsInput, FindAllPostsOutput } from './dtos/findAllPosts.dto';
 import { FindPostByIdInput, FindPostByIdOutput } from './dtos/findPostById.dto';
 import { CategoryRepository } from './repositories/category.repository';
@@ -43,6 +44,34 @@ export class PostService {
       const post = await this.postRepository.findOneBy({ id });
       if (!post) return { success: false, error: `Post ${id} not found` };
       return { success: true, post };
+    } catch {
+      return InternalServerErrorOutput;
+    }
+  }
+
+  async editPost(
+    userId: number,
+    editPostInput: EditPostInput,
+  ): Promise<EditPostOutput> {
+    try {
+      const post = await this.postRepository.findOne({
+        where: {
+          id: editPostInput.postId,
+        },
+        loadRelationIds: true,
+      });
+      if (post.authorId !== userId)
+        return { success: false, error: 'Permission Denied' };
+      if (!post)
+        return {
+          success: false,
+          error: `Post ${editPostInput.postId} Not found`,
+        };
+      await this.postRepository.save({
+        id: editPostInput.postId,
+        ...editPostInput,
+      });
+      return { success: true };
     } catch {
       return InternalServerErrorOutput;
     }
